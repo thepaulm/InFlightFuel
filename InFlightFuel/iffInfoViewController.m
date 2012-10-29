031dc50a9398068e15c19cbcafe22e4bbd838077
Paul Mikesell <paulm@paulm-laptop.local>
2012-10-28 20:39:08 -0700

    - rewrote help text

--- InFlightFuel/iffInfoViewController.m
+++ InFlightFuel/iffInfoViewController.m
@@ -32,14 +32,15 @@
     [self.text setNumberOfLines:0];
     [self.text setText:
 @"Welcome to InFlightFuel!\n\n"
-"    The purpose of this app is to help in fuel planning and projections for tank switching.  When InFlight mode "
-"is off, you can set the tank levels to Tabs for Full, or manually set the levels with the fuel slider. "
-"You can also enter the options window and set the Tabs and Full levels, as well as the target difference "
-"for the levels between tanks.  These settings will be remembered.\n\n"
-"    When InFlight mode is On, all of the controls other than the main slider and tank switch are disabled. "
-"This is to prevent accidentally resetting your fuel levels while in Flight.  In Flight mode will also "
-"enable tracking of your previous cumulative tank switch levels, as well as projections for when you "
-"should switch to maintain your target tank differential.  Don't worry if you switch early or late, "
+"    The purpose of this app is to help in fuel planning and projections for tank switching.\n\n"
+"Mode of Operation:\n\n"
+"    With the InFlight switch set to \"Off\", use the Tabs or Full buttons, or manually set your "
+"starting fuel levels with the slider.  Once you have your initial starting fuel levels set, "
+"switch the InFlight switch to \"On\" before you begin your flight.\n\n"
+"    With the InFlight switch set to \"On\", InFlightFuel will track your in flight tank switches "
+"as well as make projections about when (in terms of total fuel used) you should switch to "
+"maintain your target tank differential.  The pink line is showing your current tank use segment "
+"and the black lines show historical tank switches.  Don't worry if you switch early or late, "
 "we will recalculate the next switch projection on the fly.\n\n"
 "    Comments, suggestions, or bug reports to pmikesell@gmail.com"];
 
